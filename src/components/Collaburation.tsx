01d3740612cebfe6400b2894c893ff04c8f46704
M Subash <105859085+Git-Subash@users.noreply.github.com>
2024-10-16 23:44:39 +0530

    fast

--- src/components/Collaburation.tsx
+++ src/components/Collaburation.tsx
@@ -4,7 +4,7 @@ import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 export default function Collaburation() {
   return (
     <div className="relative flex h-24 flex-col items-center justify-center overflow-hidden rounded-md bg-white antialiased dark:bg-black">
-      <InfiniteMovingCards items={Logos} speed="fast" />
+      <InfiniteMovingCards items={Logos} speed="fast" direction="right" />
     </div>
   );
 }
