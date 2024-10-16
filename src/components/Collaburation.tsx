bc4afff5218a6194c1761382d07754140a5ddebd
M Subash <105859085+Git-Subash@users.noreply.github.com>
2024-10-16 20:55:28 +0530

    infinite moving cards updated

--- src/components/Collaburation.tsx
+++ src/components/Collaburation.tsx
@@ -1,11 +1,10 @@
-"use client";
 import { Logos } from "@/constants/detailes";
 import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 
 export default function Collaburation() {
   return (
-    <div className="dark:bg-grid-white/[0.05] relative flex h-24 flex-col items-center justify-center overflow-hidden rounded-md bg-white antialiased dark:bg-black">
-      <InfiniteMovingCards items={Logos} direction="right" speed="fast" />
+    <div className="relative flex h-24 flex-col items-center justify-center overflow-hidden rounded-md bg-white antialiased dark:bg-black">
+      <InfiniteMovingCards items={Logos} speed="normal" />
     </div>
   );
 }
